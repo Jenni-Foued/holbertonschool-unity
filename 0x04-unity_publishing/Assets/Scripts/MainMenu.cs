a53b260b49a77c70b1ae30b78a350cbf1f1d73fe
Jenni <medfouedjenni@gmail.com>
2021-08-12 13:33:19 +0100

    Fix color blind mode toggle

--- 0x04-unity_publishing/Assets/Scripts/MainMenu.cs
+++ 0x04-unity_publishing/Assets/Scripts/MainMenu.cs
@@ -18,6 +18,11 @@ public class MainMenu : MonoBehaviour
             trapMat.color = new Color32(255, 112, 0, 1);
             goalMat.color = Color.blue;
         }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
         SceneManager.LoadScene("Maze");
     }
 
