40d5a718312396d61f422f787b527fb71182838f
Jenni <medfouedjenni@gmail.com>
2021-09-09 14:04:52 +0100

    Update movements

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -4,43 +4,77 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float horizontalInput;
-    float verticalInput;
-    float jumpForce = 10f;
-    float speed = 10f;
-    Rigidbody rb;
-    bool isGrounded;
-    // Start is called before the first frame update
+    private CharacterController controller;
+    private float verticalVelocity;
+    private float groundedTimer;        // to allow jumping when going down ramps
+    private float playerSpeed = 10.0f;
+    private float jumpHeight = 5.0f;
+    private float gravityValue = 9.81f;
+ 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        controller = gameObject.GetComponent<CharacterController>();
     }
-
-    // Update is called once per frame
+ 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        verticalInput = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        bool groundedPlayer = controller.isGrounded;
+        if (groundedPlayer)
         {
-            rb.AddForce(Vector3.up * jumpForce * Time.deltaTime, ForceMode.VelocityChange);
+            // cooldown interval to allow reliable jumping even whem coming down ramps
+            groundedTimer = 0.2f;
         }
-        transform.Translate(horizontalInput, 0, verticalInput);
-
-        // Restart the game if the player falls
-        if(transform.position.y < -30)
+        if (groundedTimer > 0)
         {
-            transform.position = new Vector3(0, 30, 0);
+            groundedTimer -= Time.deltaTime;
         }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        isGrounded = true;
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        isGrounded = false;
+ 
+        // slam into the ground
+        if (groundedPlayer && verticalVelocity < 0)
+        {
+            // hit ground
+            verticalVelocity = 0f;
+        }
+ 
+        // apply gravity always, to let us track down ramps properly
+        verticalVelocity -= gravityValue * Time.deltaTime;
+ 
+        // gather lateral input control
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+ 
+        // scale by speed
+        move *= playerSpeed;
+ 
+        // only align to motion if we are providing enough input
+        if (move.magnitude > 0.05f)
+        {
+            gameObject.transform.forward = move;
+        }
+ 
+        // allow jump as long as the player is on the ground
+        if (Input.GetButtonDown("Jump"))
+        {
+            // must have been grounded recently to allow jump
+            if (groundedTimer > 0)
+            {
+                // no more until we recontact ground
+                groundedTimer = 0;
+ 
+                // Physics dynamics formula for calculating jump up velocity based on height and gravity
+                verticalVelocity += Mathf.Sqrt(jumpHeight * 2 * gravityValue);
+            }
+        }
+ 
+        // inject Y velocity before we use it
+        move.y = verticalVelocity;
+ 
+        // call .Move() once only
+        controller.Move(move * Time.deltaTime);
+    
+        // reset the game
+        if(transform.position.y < -30.0f)
+        {
+            transform.position = new Vector3(0.0f, 30.0f, 0.0f);
+        }
     }
 }
