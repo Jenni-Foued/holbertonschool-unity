a540302c49e5f24407a47ac60353116b6623636b
Jenni <medfouedjenni@gmail.com>
2021-09-09 22:34:33 +0100

    Time trial

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -13,7 +13,6 @@ public class PlayerController : MonoBehaviour
     {
         controller = gameObject.GetComponent<CharacterController>();
     }
-
     void Update()
     {
         bool groundedPlayer = controller.isGrounded;
