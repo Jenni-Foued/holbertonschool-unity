09decde80144a933c36f659b548ddfb30c9b07e2
Jenni <medfouedjenni@gmail.com>
2022-03-25 10:06:04 +0100

    WinCanvas scripting

--- 0x06-unity-assets_ui/Assets/Scripts/Timer.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/Timer.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField]Text timerText;
+    [SerializeField] Text timerText;
+    [SerializeField] Text finalTimeText;
     float startTime;
 
 
@@ -20,5 +21,10 @@ public class Timer : MonoBehaviour
         string minutes = ((int)timer / 60).ToString();
         string seconds = (timer % 60).ToString("f2");
         timerText.text = minutes + ":" + seconds;
+    }
+
+    public void Win()
+    {
+        finalTimeText.text = timerText.text;
     }
 }
--- 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -5,9 +5,11 @@ public class WinTrigger : MonoBehaviour
 {
     [SerializeField] Text timerText;
     public GameObject winCanvas;
+    public Timer timer;
 
     void Start()
     {
+        timer = FindObjectOfType<Timer>();
     }
 
     // OnTriggerEnter is called when the gameObject is triggered
@@ -20,5 +22,6 @@ public class WinTrigger : MonoBehaviour
         timerText.fontSize = 60;
         // Change TimerText's color to green
         timerText.color = Color.green;
+        timer.Win();
     }
 }
